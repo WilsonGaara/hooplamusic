955f72547085d37f0ab337cdc10a40a53f7eb5fb
WilsonGaara <37121741+WilsonGaara@users.noreply.github.com>
2018-03-30 13:30:36 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 
-exports.TOKEN = 'process.env.BOT_TOKEN';
+//exports.TOKEN = 'process.env.BOT_TOKEN';
 
 exports.PREFIX = 'H.';
 
-exports.GOOGLE_API_KEY = 'process.env.YT_API';
+//exports.GOOGLE_API_KEY = 'process.env.GOOGLE';
