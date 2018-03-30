04ea704d00b8567927d63feb95b14ff71e0dd64d
WilsonGaara <37121741+WilsonGaara@users.noreply.github.com>
2018-03-30 18:24:14 -0300

    Update MusicBot.js

--- MusicBot.js
+++ MusicBot.js
@@ -87,14 +87,9 @@ msg.channel.send({embed: embed})
 	} else if (command === `pular`) {
 		if  (!msg.member.hasPermissions(["MANAGE_GUILD"])) return msg.reply("<:blobastonished:395358298968424448> **l** Desculpe, porém você não tem permissão para usar este comando bobinho(a). Por isso criei a limitação DJ, para mais informações. Use `"+PREFIX+"dj`");
 		if (!msg.member.voiceChannel) return msg.channel.send(':x: **l** Você não ficou em um canal de voz!');
-		if (!serverQueue) return msg.channel.send('Nada tocando...');
-		msg.reply(`a música foi pulada.`)
-
+		if (!serverQueue) return msg.channel.send('Nada tocando...')
 		serverQueue.connection.dispatcher.end();
-		
 		return undefined;
-
-
 
 
 	} else if (command === `ping`) {
